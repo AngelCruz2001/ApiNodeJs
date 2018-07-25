1b9581cb5c9f7a931cd537bdf08e08f9f58a8d06
AERM2018 <retana.martinez.angel.eduardogmail.com>
2018-07-25 10:36:31 -0500

    Funciona todo

--- server/controllers/Lugar&Usuario.js
+++ server/controllers/Lugar&Usuario.js
@@ -1,51 +1,63 @@
 var {modelUsuarios}=require('../models/UsuariosModel');
 var {modelLugares}=require('../models/LugaresModel');
 var {modelLugaresFav}=require('../models/LugaresFavoritos');
-
-var FindLugar=(Nom,UserName)=>{
-    var id1,id2;
-    return new Promise((resolve,reject)=>{
-        modelLugares.find({Nombre:Nom},{_id:1},(error,idLugar)=>{
-            if(error)res.json(error);
-            else{
-                console.log("ID LUGAR"+idLugar[0]._id);
-                id1=idLugar[0]._id
-                // resolve(idLugar[0]._id);
-            }
-        }); 
-        modelUsuarios.find({UserName:UserName},{_id:1},(error,idUsuario)=>{
-                        if(error)res.json(error);
-                        else{
-                            console.log("ID USUARIO"+idUsuario[0]._id);
-                            id2=idUsuario[0]._id
-                            var ids=[id1,id2]
-                            resolve(ids);
-                        }
-            
-                    })
-    })
+var id1,id2;
+var FindLugar=(Nom)=>{
+        return new Promise((resolve,reject)=>{
+            modelLugares.find({Nombre:Nom},{_id:1},(error,idLugar)=>{
+                if(error)res.json(error);
+                else{
+                    if(idLugar[0]!==undefined){
+                        console.log("ID LUGAR"+idLugar[0]._id);
+                        id1=idLugar[0]._id;
+                    }else{
+                        id1=undefined;
+                    }
+                    resolve(id1);
+                }
+            }); 
+        })
+    
+}
+var FindUsuario=(UserName)=>{
+        return new Promise((resolve,reject)=>{
+            modelUsuarios.find({UserName:UserName},{_id:1},(error,idUsuario)=>{
+                if(error)res.json(error);
+                else{
+                    if(idUsuario[0]!==undefined){
+                        console.log("ID USUARIO"+idUsuario[0]._id);
+                        id2=idUsuario[0]._id
+                    }else{
+                        id2=undefined;
+                    }
+                    resolve(id2);
+                }
+    
+            })
+        });
+ 
 }
 var FindLugFav=(_id)=>{
-    console.log("id del lugar=>..."+_id)
     return new Promise((resolve,reject)=>{
         modelLugaresFav.find({Nombre:_id},(error,LugarFav)=>{
             if(error)res.json(error);
             else{
-                console.log(LugarFav[0]);
-                resolve(LugarFav);
+                resolve(LugarFav[0]);
             }
         });
     });
 }
 exports.getLugarFav=async(_id)=>{
     let LugFav= await FindLugFav(_id);
-    console.log(LugFav);
-    
     return LugFav;
 }
-exports.buscar=async(Nom,UserName)=>{
-    let ids=await FindLugar(Nom,UserName);
-    return ids;
+exports.buscarIdLugar=async(Nom)=>{
+    let idL=await FindLugar(Nom);
+    return idL;
+}
+exports.buscarIdUsuario=async(UserName)=>{
+    let idU=await FindUsuario(UserName);
+    return idU;
 }
 
 // exports.FindUsuario=(UserName)=>{
--- server/models/LugaresFavoritos.js
+++ server/models/LugaresFavoritos.js
@@ -3,7 +3,8 @@ var Schema=mongoose.Schema;
 // var {modelUsuarios}=require('./UsuariosModel');
 // var {modelLugares}=require('./LugaresModel');
 var LugaresFavShema = new Schema({
-    Nombre:{type:Schema.Types.ObjectId,ref:"Lugares"},
+    Nombre:{type:Schema.Types.ObjectId,ref:"modelUsuarios"},
     UserName:{type:Schema.Types.ObjectId,ref:"Usuarios"}
 })
 exports.modelLugaresFav=mongoose.model('LugaresFav',LugaresFavShema);
+//falta hacer que funcione el populate!!
